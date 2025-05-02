a26954cd3c30489dc5f52775fad6f6eebc48735d
Yura <yurikorock777@gmail.com>
2025-05-02 11:48:00 +0300

    start

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,22 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
 
 const Form = () => {
-  return <h2>Form</h2>;
+  return (
+    <form className={style.form}>
+      <button className={style.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <input
+        className={style.input}
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        autoFocus
+      />
+    </form>
+  );
 };
 
 export default Form;
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,8 +1,13 @@
 import Text from '../components/Text/Text';
+import Form from '../components/Form/Form';
 
 const Todos = () => {
+  const addNewTodo = inputValue => {
+    console.log(inputValue);
+  };
   return (
     <>
+      <Form onSubmit={addNewTodo} />
       <Text textAlign="center">There are no any todos ...</Text>
     </>
   );
